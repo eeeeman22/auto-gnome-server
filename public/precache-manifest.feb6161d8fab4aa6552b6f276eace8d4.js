2692eeee5038c9f0f3383c92fadfcef98d07e529
eeeeman <eeeeman@icloud.com>
2020-01-17 13:22:04 -0500

    first commit  for serfver

---
+++ public/precache-manifest.feb6161d8fab4aa6552b6f276eace8d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbb2efa6645a50b4fc4db7f046c1e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08d7af936847dff5b982",
+    "url": "/static/css/main.f33552f3.chunk.css"
+  },
+  {
+    "revision": "dfac0ed6eea101032e71",
+    "url": "/static/js/2.04a48357.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.04a48357.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08d7af936847dff5b982",
+    "url": "/static/js/main.adab5e4a.chunk.js"
+  },
+  {
+    "revision": "0a00dcd9948549ab1dc7",
+    "url": "/static/js/runtime-main.c7c6c9c6.js"
+  },
+  {
+    "revision": "fd742d0fd75922e948650fcb5bceb0ba",
+    "url": "/static/media/metronome1.fd742d0f.flac"
+  }
+]);
